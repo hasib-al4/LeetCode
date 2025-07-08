84c8befa3d26d988e8f86a631a4add60291f4471
Hasib Ali <hasibalivpindia@gmail.com>
2025-07-08 09:57:19 +0530

    Time: 0 ms (100%), Space: 9.5 MB (91.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+        if(s.size() != t.size()) return false;
+        vector<int> freq(26,0);
+        for(char ch : s) {
+            freq[ch - 'a']++;
+        }
+        for(char ch : t) {
+            freq[ch - 'a']--;
+        }
+        for(int cnt : freq) {
+            if(cnt != 0) return false;
+        }
+        return true;
     }
 };
